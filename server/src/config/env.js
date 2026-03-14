3b8c1fa3141ae490f4038b5bfe9dbc39265e8b1a
Kovács Zsombor <125221225+KoZsombat@users.noreply.github.com>
2026-03-14 21:48:05 +0100

    modified env

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -5,6 +5,7 @@ dotenv.config();
 export const env = {
   PORT: process.env.PORT || 3001,
   FRONTEND_URL: process.env.FRONTEND_URL,
+  BACKEND_URL: process.env.BACKEND_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   SESSION_SECRET: process.env.SESSION_SECRET,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
